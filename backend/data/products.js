dfde690782afb9a00ac82c88bb48d36b840856d9
Suyash Patil <suyashpatil532001@gmail.com>
2021-05-02 22:46:02 +0530

    backend initiated with express

---
+++ backend/data/products.js
@@ -0,0 +1,84 @@
+const products = [
+{
+  _id: '1',
+  name: "Docker",
+  image: '/images/docker.jpg',
+  description: "learn docker",
+  price: 30,
+  countInStock: 2,
+  rating: 5,
+  numReviews: 5
+},
+{
+  _id: '2',
+  name: "Java",
+  image: '/images/java.jpg',
+  description: "learn java",
+  price: 30,
+  countInStock: 2,
+  rating: 3.4,
+  numReviews: 5
+},
+{
+  _id: '3',
+  name: "Python",
+  image: '/images/automate-python.jpg',
+  description: "learn python",
+  price: 30,
+  countInStock: 2,
+  rating: 4.6,
+  numReviews: 5
+},
+{
+  _id: '4',
+  name: "DSA",
+  image: '/images/dsa.jpg',
+  description: "learn DSA",
+  price: 30,
+  countInStock: 2,
+  rating: 2.7,
+  numReviews: 5
+},
+{
+  _id: '5',
+  name: "Coding Interview",
+  image: '/images/coding-interview.jpg',
+  description: "crack interview",
+  price: 30,
+  countInStock: 2,
+  rating: 4.7,
+  numReviews: 5
+},
+{
+  _id: '6',
+  name: "Kubernetes",
+  image: '/images/kubernetes.jpg',
+  description: "learn kubernetes",
+  price: 30,
+  countInStock: 2,
+  rating: 3.5,
+  numReviews: 5
+},
+{
+  _id: '7',
+  name: "React Full Stack",
+  image: '/images/react-fullstack.jpg',
+  description: "React fullstack",
+  price: 30,
+  countInStock: 2,
+  rating: 3.2,
+  numReviews: 5
+},
+{
+  _id: '8',
+  name: "React Hooks",
+  image: '/images/react-hooks.jpg',
+  description: "learn hooks",
+  price: 30,
+  countInStock: 2,
+  rating: 4,
+  numReviews: 5
+}
+]
+
+module.exports = products
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/api/products',(req,res) => {
+  res.json(products)
+})
+
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(5000, console.log('Server is running at port 5000'))
