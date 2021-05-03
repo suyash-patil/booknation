8b6935dfa2b66361f3fcbed47d71c084a2bdc778
Suyash Patil <suyashpatil532001@gmail.com>
2021-05-03 13:09:48 +0530

    mongodb with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
