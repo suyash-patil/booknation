307b6509d6025e6d1a4d2d48213be374931d96d0
Suyash Patil <suyashpatil532001@gmail.com>
2021-05-03 14:41:16 +0530

    renamed productRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import productRoutes from './routes/producRoutes.js'
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
 
 connectDB()
