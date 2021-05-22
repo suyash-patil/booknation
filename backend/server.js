0f4d748f8e0ce44632605b950dfea07ad30481a5
Suyash Patil <suyashpatil532001@gmail.com>
2021-05-22 23:51:49 +0530

    production problem fix

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import morgan from 'morgan'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import path from 'path'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import bodyParser from 'body-parser'
@@ -44,6 +45,18 @@ app.post('/api/create-pdf',(req,res) => {
 app.get('/api/fetch-pdf',(req,res) => {
   res.sendFile('result.pdf',{root: '.'})
 })
+
+const __dirname = path.resolve()
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+} else {
+  app.get('/', (req,res) => {
+    res.send('API is running...')
+  })
+}
 
 
 app.listen(process.env.PORT || 5000, console.log(`Server is running at port ${process.env.PORT}`))
